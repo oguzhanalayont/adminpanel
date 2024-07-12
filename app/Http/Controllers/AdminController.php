7aa7cd873f0fd23a9c22fa1a4633069e0099ba00
oguzhanalayont <oguzhanalayont2021@gmail.com>
2024-07-12 15:01:01 +0300

    total users count added on admin dashboard page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $userCount = User::count();
+        return view('admin.dashboard' , compact("userCount"));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,7 +2,17 @@
 
 @section('content')
 <div class="container">
-    <h1>Admin Dashboard Page</h1>
+    <h1>Admin Dashboard</h1>
     <p>Welcome To The Admin Dashboard Page</p>
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Total Users</h5>
+                    <p class="card-text display-4">{{ $userCount }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
